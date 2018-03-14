06599a1432ebb62a55d1fb6fd68b34d539fd3f30
alidarwiche <darwiche2005@utexas.edu>
2018-03-13 23:02:28 -0500

    resetting to create-react-app with google-font: roboto

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <MyAwesomeReactComponent />
-      </MuiThemeProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
