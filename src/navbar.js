724ea256cdecee245b94aad84156136540e741c3
alidarwiche <darwiche2005@utexas.edu>
2018-03-14 22:19:24 -0500

    adding navbar

---
+++ src/navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './navbar.css';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="NavBar">
+        <header className="NavBar-header">
+          <h1 className="NavBar-title">Ali M. Darwiche</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
